c2a334f86bcdddeb783dfa12e3885174ec509bc4
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2024-04-02 17:49:33 -0500

    Style changes

--- excel_data_engine/urls.py
+++ excel_data_engine/urls.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 from . import views
 
 urlpatterns = [
-    path('', views.excel_home)
+    path('', views.excel_home, name='excel_home'),
+    path('create/', views.create_excel),
+    path('update/', views.update_excel),
+    path('get/<int:id>', views.get_excel),
+    path('refresh/<int:id_sheet>', views.refresh_excel),
+    path('data/<int:id_sheet>', views.get_sheet_data)
 ]
--- excel_data_engine/views.py
+++ excel_data_engine/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Excel
 from .forms import ExcelForm
 import pandas as pd
 import math
+import json
 
 def excel_home(request):
     excel_list = list(Excel.objects.values())
     return render(request, "excel/excel.html", {"excel":"", "excel_list":excel_list, "form": ExcelForm()})
 
-def excel_app(request, name):
-    excel = Excel.objects.get(name = name)
-    if excel.refresh:
-        url = excel.url
-        df = pd.read_csv(url)
-        table = df.to_dict(orient='records')
-        jsonTable = df.to_json(orient='records')
-        excel.data = jsonTable
-        excel.refresh = False
-        excel.save()
-        print(table)
-    else:
-        table = json.loads(excel.data)
+def load_sheet_data(sheet_url):
+    df = pd.read_csv(sheet_url)
+    table = df.to_dict(orient='records')
 
     column_names = table[0].keys()
     column_names = list(column_names)
@@ -33,22 +25,70 @@ def excel_app(request, name):
                     table[index][key] = ''
             elif type(table[index][key]) == NoneType:
                 table[index][key] = ''
+    
+    return json.dumps(table)
 
-    filter_columns=[]
-    for key in column_names:
-        column_key = re.sub('[^A-Za-z0-9]+', '', key)
-        filter_columns.append(column_key)
+def refresh_excel(request, id_sheet):
+    excel = Excel.objects.get(id = id_sheet)
+    
+    excel.data = load_sheet_data(excel.url)
+    excel.records_length = len(json.loads(excel.data))
 
-    return render(request, "excel/excel-spreadsheet.html", {
-        "excel": table,
-        "column_names": column_names,
-        "id_columns": filter_columns,
-        "excel_data": excel
-    })
-
-def refresh_excel(request, name):
-    excel = Excel.objects.get(name = name)
-    excel.refresh = True
     excel.save()
 
-    return redirect(f"/excel/spreadsheet/{name}")
+    return redirect("excel_home")
+
+def create_excel(request):
+    if request.method == "POST":
+        load_images = request.POST.get("load_images", False)
+        load_images = True if load_images else False
+        excel = Excel(
+            name=request.POST["name"],
+            url=request.POST["url"],
+            loadImages=load_images
+        )
+
+        excel.data = load_sheet_data(excel.url)
+        excel.records_length = len(json.loads(excel.data))
+
+        excel.save()
+
+        return redirect("excel_home")
+
+def get_excel(request, id):
+    excel = Excel.objects.get(id=id)
+
+    excel_dict = {
+        "id": excel.id,
+        "name": excel.name,
+        "url": excel.url,
+        "loadImages": excel.loadImages,
+    }
+
+    return HttpResponse(json.dumps(excel_dict), "application/json")
+
+def update_excel(request):
+    if request.method == "POST":
+        excel = Excel.objects.get(id=request.POST["id"])
+
+        excel.name = request.POST["name"]
+        excel.url = request.POST["url"]
+
+        load_images = request.POST.get("load_images", False)
+        if load_images:
+            excel.loadImages = True
+        else:
+            excel.loadImages = load_images
+
+        excel.data = load_sheet_data(excel.url)
+        excel.records_length = len(json.loads(excel.data))
+
+        excel.save()
+
+        return redirect("excel_home")
+
+def get_sheet_data(request, id_sheet):
+    excel = Excel.objects.get(id = id_sheet)
+    data = json.loads(excel.data)
+
+    return HttpResponse(data, content_type = "application/json")
